0cc26f19e0f27736a239b81d427cd67efe9dfb4d
alexfou <alejandro.foullon@gmail.com>
2014-09-03 01:39:12 +0000

    con router esperenado subsribe del userlogged para control

--- server/publications.js
+++ server/publications.js
@@ -14,3 +14,13 @@ return Stores.find();
 Meteor.publish(null, function (){ 
   return Meteor.roles.find({})
 })
+
+Meteor.publish('currentUserLogged', function (){ 
+  if (this.userId) {
+    return Meteor.users.find(
+      {_id: this.userId},
+      {fields: {profile: 1, username: 1, emails: 1}});
+  } else {
+    return null;
+  }
+});
