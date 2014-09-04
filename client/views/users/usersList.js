f8b03abec530d4e802b6b9b10b7be3d26671f472
alexfou <alejandro.foullon@gmail.com>
2014-09-04 03:20:36 +0000

    mejoras en users roles y permisos

--- client/views/users/usersList.js
+++ client/views/users/usersList.js
@@ -1,6 +1,32 @@
+// Template.usersList.helpers({
+//   users: function(){
+//     //var users = Meteor.users.find().fetch();
+//     return Meteor.users.find().fetch();
+//   }
+// });
+
 Template.usersList.helpers({
-  users: function(){
-    //var users = Meteor.users.find().fetch();
-    return Meteor.users.find().fetch();
-  }
+  
+  usersTable: function () {
+    var users = Meteor.users.find({},{fields: {emails: 1, roles: 1}}).fetch();
+    var users2 = _.map(users, function(u){return _.extend(u,{emailList:u.emails[0].address});});
+    return users2;
+  },
+  
+  tableSettings: function () {
+        
+        var fs =[
+          { key: 'emailList', label: 'Usuario' },
+          { key: 'roles', label: 'Roles' }];
+
+    
+        return {
+            rowsPerPage: 5,
+            showFilter: true,
+            //fields: ['title', 'author'],
+            useFontAwesome: true,
+            fields: fs
+        };
+    },
+  
 });
